7ad6d97105453ccb75e3368fb7a5f577b4e0799b
Bobby Lee <rdlee8181@gmail.com>
2020-10-05 21:09:11 -0700

    Added tearDown method to test

--- tests/test-sapphire-popups.php
+++ tests/test-sapphire-popups.php
@@ -2,16 +2,21 @@
 
 class Sapphire_Popups_VerifyTest extends WP_UnitTestCase {
 	public function setUp() {
-				parent::setUp();
+		parent::setUp(); // Used for DB setup.
 
-				$this->class_instance = new Sapphire_Popups();
-		}
+		$this->class_instance = new Sapphire_Popups();
+	}
+
+	public function tearDown() {
+		unset( $this->class_instance );
+	}
 
 
 	public function test_get_plugin_name() {
-			$plugin_name = $this->class_instance->get_plugin_name();
-			$expected = 'sapphire-popups';
+		$plugin_name = $this->class_instance->get_plugin_name();
+		$expected = 'sapphire-popups';
 
-			$this->assertEquals($expected, $plugin_name);
+		$this->assertEquals($expected, $plugin_name);
 	}
+	
 }
